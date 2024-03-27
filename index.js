ddf391309561e93a5b8234443288c6c42705b760
Sedly <sedly.keith@student.moringaschool.com>
2024-03-27 08:38:15 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},]
+
+function superbowlWin (record) {
+    for (let i = 0; i < record.length; i++) {
+        if (record[i].result === "W") {
+            return record[i].year
+        }
+    }
+    return undefined
+    
+    
+
+}
+console.log(2015)
